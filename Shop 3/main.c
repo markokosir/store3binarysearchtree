28418f47c6f1a05bbbe18d307b9529c49933904e
Marko Kosir <kosir.mar@gmail.com>
2018-01-11 18:18:48 +0100

    Update main.c

--- Shop 3/main.c
+++ Shop 3/main.c
@@ -2,13 +2,7 @@
 Program: Store 3 - Binary Search Tree
 Author: Marko Kosir
 
-This is a compilation of a store programs I wrote for academic purposes using three different 
-data structures from scratch. Store 3 is implemented using binary search tree. 
-When the program starts it allocates memory for 4 products on heap. Since 
-this version is implemented using binary  search tree, therefore it is possible 
-to allocate memory during runtime. To keep things simple I decided to sort products 
-by a code value, so when displaying products on the screen, they will be printed out in
-an increasing order.
+Store 3 is implemented using binary search tree. When the program starts it allocates memory for 4 products on heap.
 
 Files included:
                 - products.txt	                sample database
@@ -20,19 +14,10 @@ Files included:
 
 Note: All the code is written in pure C programming language, however header file conio.h is
       inlcuded in this program(windows.h is also included for rendering reasons), which is 
-      not part of ANSI C standard.               
-
-This program was made entirely by Marko Kosir, it is freeware,
-feel free to use this code or change it in any way you like.
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
-without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-email: kosir.mar@gmail.com               
+      not part of ANSI C standard.                
 
 Tested - Windows XP
 Compiled - Microsoft Visual Studio 2005
-		   MVS 2005 compiler gives you warnings about some of the functions and suggest that you
-		   should use ISO C++ functions and replace deprecated ones, if you want feel free to do it.
 
 Released: July 29th, 2005
 
